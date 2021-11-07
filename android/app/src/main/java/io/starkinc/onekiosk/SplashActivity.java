b0154123f0ee54e6390e642a28f3838cab7d62ca
tanishqmanuja <tanishqmanuja@gmail.com>
2021-11-07 12:47:46 +0530

    App: Splashscreen migration to A12

--- android/app/src/main/java/io/starkinc/onekiosk/SplashActivity.java
+++ android/app/src/main/java/io/starkinc/onekiosk/SplashActivity.java
@@ -6,12 +6,19 @@ import android.os.Bundle;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.splashscreen.SplashScreen;
 
 public class SplashActivity extends AppCompatActivity {
   @Override
   protected void onCreate(@Nullable Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
-    ThemeHelper.applyBgToActivity(this);
+//    ThemeHelper.applyBgToActivity(this);
+//    Intent intent = new Intent(this, MainActivity.class);
+//    startActivity(intent);
+//    finish();
+
+    // Handle the splash screen transition.
+    SplashScreen splashScreen = SplashScreen.installSplashScreen(this);
     Intent intent = new Intent(this, MainActivity.class);
     startActivity(intent);
     finish();
