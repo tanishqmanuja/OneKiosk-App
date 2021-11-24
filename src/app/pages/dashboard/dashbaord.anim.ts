b090583e8b24eb6f09cade28b9f0f2b191fca4b8
tanishqmanuja <tanishqmanuja@gmail.com>
2021-11-24 11:23:25 +0530

    Dashboard Page: Tweaked anims

--- src/app/pages/dashboard/dashbaord.anim.ts
+++ src/app/pages/dashboard/dashbaord.anim.ts
@@ -32,9 +32,41 @@ export const animations = [
       ),
     ]),
   ]),
-  trigger('card', [
+  trigger('classes', [
     transition(':enter', [
-      query('.class-card', style({ opacity: 0 }), { optional: true }),
+      query('.class-card,.bigtitle,.title', style({ opacity: 0 }), {
+        optional: true,
+      }),
+      query(
+        '.bigtitle',
+        animate(
+          '280ms ease-out',
+          keyframes([
+            style({
+              opacity: 0,
+              transform: 'translateX(-27px)',
+              offset: 0,
+            }),
+            style({ opacity: 1, transform: 'translateX(0px)', offset: 1 }),
+          ])
+        ),
+        { optional: true }
+      ),
+      query(
+        '.title',
+        animate(
+          '220ms ease-out',
+          keyframes([
+            style({
+              opacity: 0,
+              transform: 'translateX(-10px)',
+              offset: 0,
+            }),
+            style({ opacity: 1, transform: 'translateX(0px)', offset: 1 }),
+          ])
+        ),
+        { optional: true }
+      ),
       query(
         '.class-card',
         stagger(
